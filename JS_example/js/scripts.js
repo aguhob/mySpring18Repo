db3e86d31fdc5952bb7ab5d1a59ec2cb033611d6
Minda Aguhob <minan@Mindas-MacBook-Pro.local>
2018-02-21 13:16:14 -0800

    mindaaguhob.com and intro MT updates

--- JS_example/js/scripts.js
+++ JS_example/js/scripts.js
@@ -5,7 +5,7 @@ JS */
 
 var a = 100;
 var b = 1;
-/* var sum = a + b; */
+var sum = a + b;
 
 var c = 'hi'
 var d = true
@@ -20,7 +20,88 @@ if (kathyAge > 21) {
 	console.log("Take a Drink");
 }
 
+/* 2/20/18 notes missed */
+
+age = 100
+
+if (age == 101) {
+        console.log('Take a drink');
+}       else {
+        console.log('You are too young');
+}
 
 console.log(a, b, c, d);  // Adds sum to console (Inspect or alt-option-I > click tab "Console") 
 
+/* 2/21/18 Notes */
+
+/* console.log(a, b);
+
+    var count = 0;
+
+while (count < 20) {
+    
+    console.log('The current loop is ' + count + ".");
+    console.log(a, b);
+    /* count = count + 1; 
+    count++; /* does same as above 
+    
+}
+    
+
+var count = 0;
+var firstLine = "*";
+
+while (count < 20) {
+    
+    firstLine = firstLine + "*";
+    console.log(firstLine);
+    count++; 
+    
+}
+
+///need to debug below. infinite loop///
+
+//var count = 100;
+//var firstLine = "*";
+//
+//while (count > 0) {
+//    
+//    console.log(firstLine);
+//    firstLine = firstLine + "*";
+//    
+//}
+//    /* '===' same value and same type */
+//    
+//        if (count % 2 === 1) {
+//        firstLine = firstLine + " ";
+//    
+//        count--; /*subtracts 1
+//    }
+
+    
+
+///need to debug below. infinite loop///
+
+//var count = 100;
+//var firstLine = "";
+//var end = true;
+//
+//while (count > 0 && end) {
+//
+//    console.log("before while loop");
+//    
+//    if (count >= 0 && <= 20) {
+//        count++;
+//        console.log(firstLine.repeat(count));
+//        
+//    } else if (count > 20) {
+//        count--;
+//        console.log(firstline.repeat(count));
+//    }
+//      else if (count ==0) {
+//        end=false;
+//        
+//    } 
+    
+for loops
 
---
+++ JS_example/js/scripts2.js
@@ -0,0 +1,77 @@
+///templates. counter:
+
+//
+//var count = 0;
+//
+//while (count < 10) {
+//    
+//    count = count + 1; 
+//    number = 10;
+//    
+//}
+//
+/////3 sections, for loop:
+//
+//
+//for (var count = 0; count < 10; count++) {
+//    
+//    console.log("Hello");
+//    
+//}
+//
+/////less common
+//
+//do {
+//
+//    console.log("True");
+//
+//} while (false); 
+//
+//var a = 1
+//var b = 2
+//var c = "hi"
+//var d = "hi again"
+//var e = true
+
+//var a = 1, b = 2, c = "hi", d = "hi again", e = true;
+
+// count to 20, using number variable that doesn't change value. don't forget console log
+
+//
+//var a = 0;
+//
+//while (a < 20) {
+//
+//    a = a + 1; 
+//    console."log(a);
+//}
+
+///shorter version of code; counting for "each" count!
+
+//for (var a = 0; a < 20; a = a + 1) {
+//    console.log(a);
+//
+//    for (var e = 0; e < 1; e++) {
+//        console.log("hi");
+//    }
+//}
+
+///Arrays with square brackets. Use double quotes for all strings.
+
+//var array1 = ["sun", "mon", "tue", "wed", "th", "fri", "sat"];
+//
+//array1[3] = "replaced"; //replaces data
+//var crr = array1[3]; //pull data
+//
+//for (var i = 0; i < 7; i++) {
+//    console.log(array1[i]);
+//}
+
+var array5 = ["sun", "mon", "tue", "wed", "th"];
+
+for (var i = 0; i < array5.length; i++) {
+    console.log(array5[i]);
+}
+
+
+
