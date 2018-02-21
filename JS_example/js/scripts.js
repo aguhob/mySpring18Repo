da31f307e57b2f2df19ffe9adcd6ef74e4ba8c04
Minda Aguhob <minan@Mindas-MacBook-Pro.local>
2018-02-21 09:31:14 -0800

    updated stylesheet: copied fr Intro MTechies sheet

--- JS_example/js/scripts.js
+++ JS_example/js/scripts.js
@@ -3,8 +3,24 @@
 comments in 
 JS */
 
-var a = 1;
+var a = 100;
+var b = 1;
+/* var sum = a + b; */
 
-// this variable is a class example
+var c = 'hi'
+var d = true
+
+/* alert(sum); // Pop up window for sum */
+
+if (true) { console.log("hello"); }
+
+var kathyAge =30
+
+if (kathyAge > 21) {
+	console.log("Take a Drink");
+}
+
+
+console.log(a, b, c, d);  // Adds sum to console (Inspect or alt-option-I > click tab "Console") 
 
 
